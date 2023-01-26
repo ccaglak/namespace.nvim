9ffae1c1d8babf009581f2bd3e5175bedd57a53e
ccaglak <ccaglak@gmail.com>
2023-01-26 04:42:16 +0300

    temp fix for root dir

--- lua/namespace/rootDir.lua
+++ lua/namespace/rootDir.lua
@@ -1,13 +1,40 @@
-local Job = require("plenary.job")
+-- borrowd form lazyvim https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/util/init.lua
 
 local M = {}
-M.searchRootDir = function()
-    local fd = Job:new({
-        command = 'fd',
-        args = { "-a", "composer.json", "-E", "node_modules", "-E", "vendor" },
-    })
-    fd:sync()
-    return unpack(fd:result()):gsub("composer.json", "")
+M.root_patterns = { ".git", "lua", "vendor", "node_modules" }
+function M.searchRootDir()
+    ---@type string?
+    local path = vim.api.nvim_buf_get_name(0)
+    path = path ~= "" and vim.loop.fs_realpath(path) or nil
+    ---@type string[]
+    local roots = {}
+    if path then
+        for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+            local workspace = client.config.workspace_folders
+            local paths = workspace and vim.tbl_map(function(ws)
+                return vim.uri_to_fname(ws.uri)
+            end, workspace) or client.config.root_dir and { client.config.root_dir } or {}
+            for _, p in ipairs(paths) do
+                local r = vim.loop.fs_realpath(p)
+                if path:find(r, 1, true) then
+                    roots[#roots + 1] = r
+                end
+            end
+        end
+    end
+    table.sort(roots, function(a, b)
+        return #a > #b
+    end)
+    ---@type string?
+    local root = roots[1]
+    if not root then
+        path = path and vim.fs.dirname(path) or vim.loop.cwd()
+        ---@type string?
+        root = vim.fs.find(M.root_patterns, { path = path, upward = true })[1]
+        root = root and vim.fs.dirname(root) or vim.loop.cwd()
+    end
+    ---@cast root string
+    return root .. "/"
 end
 
 return M
