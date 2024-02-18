cd829e77a3abc87358bb1867e0b05bb62522542c
ccaglak <ccaglak@gmail.com>
2024-02-18 03:51:22 +0300

    bug: due to treesitter updates

--- lua/namespace/treesitter.lua
+++ lua/namespace/treesitter.lua
@@ -130,7 +130,7 @@ M.composer_search_parse = function(sr)
         "php",
         [[
 (array_element_initializer
-  (string (string_value) @sv1)
+  ((string) @sv1)
    (binary_expression right: (string (string_value) @sv2 ))
   )
   ]]
@@ -138,6 +138,7 @@ M.composer_search_parse = function(sr)
     for _, captures, _ in query:iter_matches(root, bufnr) do
         local ns = ts.get_node_text(captures[1], bufnr)
         local source = ts.get_node_text(captures[2], bufnr) -- gets the file path for future projects
+        ns = ns:sub(2):sub(1, -2)
         searched:insert(1, ns)
     end
     vim.api.nvim_buf_delete(bufnr, { force = true })
